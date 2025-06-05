e0415d8b8c5299ae062337c49e8b6ab42d2ec7ca
PopoGonry <59679349+PopoGonry@users.noreply.github.com>
2025-06-05 17:57:41 +0900

    2025-06-05
    ReplyRepository Interface 완료

--- src/main/java/com/popogonry/notid/reply/replyRepository/ReplyRepository.java
+++ src/main/java/com/popogonry/notid/reply/replyRepository/ReplyRepository.java
@@ -1,4 +1,24 @@
 package com.popogonry.notid.reply.replyRepository;
 
+import com.popogonry.notid.notice.Notice;
+import com.popogonry.notid.reply.Reply;
+
+import java.util.HashSet;
+
 public interface ReplyRepository {
+    void addReplyData(Reply reply);
+    Reply getReplyData(long replyId);
+    boolean hasReplyData(long replyId);
+    void removeReplyData(long replyId);
+
+    void addNoticeReplyData(long noticeId, long replyId);
+    boolean hasNoticeReplyData(long noticeId, long replyId);
+    void removeNoticeReplyData(long noticeId, long replyId);
+
+    void addNoticeReplySetData(long noticeId, HashSet<Long> replySet);
+    HashSet<Long> getNoticeReplySetData(long noticeId);
+    boolean hasNoticeReplySetData(long noticeId);
+    void removeNoticeReplySetData(long noticeId);
+
+    void clearAll();
 }
